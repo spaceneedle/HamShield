0def578c6ef342d3a0d08b58080d62d5332762e0
Casey Halverson <spaceneede@gmail.com>
2015-04-28 21:04:24 -0700

    new test pattern function

--- SSTV/SSTV.ino
+++ SSTV/SSTV.ino
@@ -25,21 +25,21 @@ void setup() {
   Serial.println(result);
   radio.initialize();
   radio.setFrequency(446000);
+  radio.setModeReceive();
 }
 
 /* main program loop */
 
 
 void loop() {
-     if(radio.waitForChannel(0,2000)) {        // Wait forever for calling frequency to open, then wait 2 seconds for breakers 
-       radio.setModeTransmit();                // Turn on the transmitter
-       delay(250);                             // Wait a moment
-       radio.morseOut(CALLSIGN);               // Identify our transmission
-       delay(1000);                            // Wait a second
-       radio.SSTVVISCode(ROBOT8BW);             // Send the VIS header for SC2-180
-       // SSTV TO DO 
-     } else { delay(30000); }                  // someone broke in fast after prior transmission, was it an emergency? wait 30 secs.
+     if(radio.waitForChannel(1000,2000)) {        // Wait forever for calling frequency to open, then wait 2 seconds for breakers 
+       radio.setModeTransmit();                   // Turn on the transmitter
+       delay(250);                               // Wait a moment
+       radio.SSTVTestPattern(MARTIN1);            // send a MARTIN1 test pattern
+       delay(250);
+       radio.setModeReceive();                    // Turn off the transmitter
+     } else { delay(30000); }                     // someone broke in fast after prior transmission, was it an emergency? wait 30 secs.
      
-     delay(10000);
+     delay(60000);                                // Wait a minute
 }
 
